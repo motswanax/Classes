74e245b13288850addff656440a6220848e9dfc6
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-30 14:34:38 +0200

    Demo of static members in classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = Person.Parse("Dre");
+            p.Introduce("Dom");
         }
     }
 }
