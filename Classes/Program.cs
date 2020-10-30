9eb599131ec066661ba2d22a1135c29904d8fc14
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-30 15:13:29 +0200

    Demo of the object initiallzer notation.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,6 +7,12 @@ namespace Classes
         static void Main(string[] args)
         {
             var customer = new Customer(1, "BAD");
+            // Object initializer notation. Eliminates the need for many consructors.
+            var cust = new Customer
+            {
+                Id = 123432134,
+                Name = "BAD"
+            };
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
         }
